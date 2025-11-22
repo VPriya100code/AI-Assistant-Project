3a1aa9162841b330cf4d6f5f0cc0f0d7886d7d9b
VPriya100code <priyadharshiniv.cse2024@citchennai.net>
2025-11-22 14:22:45 +0530

    Fix prompt function calls in assistant_logic.py
    Updated prompt function calls to use user_input directly.

--- assistant_logic.py
+++ assistant_logic.py
@@ -1,11 +1,11 @@
-import os 
+import os
 from groq import Groq
 from prompts import question_prompt, summary_prompt, creative_prompt
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def answer_question(user_input):
-    prompt = question_prompt.format(input=user_input)
+    prompt = question_prompt(user_input)   # <-- FIXED
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=[{"role": "user", "content": prompt}]
@@ -14,7 +14,7 @@ def answer_question(user_input):
 
 
 def summarize_text(user_input):
-    prompt = summary_prompt.format(input=user_input)
+    prompt = summary_prompt(user_input)    # <-- FIXED
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=[{"role": "user", "content": prompt}]
@@ -23,10 +23,9 @@ def summarize_text(user_input):
 
 
 def generate_creative(user_input):
-    prompt = creative_prompt.format(input=user_input)
+    prompt = creative_prompt(user_input)   # <-- FIXED
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=[{"role": "user", "content": prompt}]
     )
     return response.choices[0].message.content
-
